a67ec28183332227db28ded46a1547a2fb1c9855
Eduardo Motton <emotton@gmail.com>
2021-12-09 20:39:49 -0300

    Inclusão

---
+++ vue-ts/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1>HELLO</h1>
+    <div>{{msg}}</div>
+    <div>{{nome}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HelloWorld extends Vue {
+  @Prop() private msg!: string;
+  get nome(){
+    return this.$store.state.nome;
+  }
+}
+</script>
---
+++ vue-ts/src/store/index.ts
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    nome: "Eduardo F Motton"
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
