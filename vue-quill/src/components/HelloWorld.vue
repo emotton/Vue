fac6973a2c61b127ff5c4e2ed481450d55a081b6
Eduardo Motton <emotton@gmail.com>
2022-02-10 19:17:22 -0300

    Atualizacao

--- vue-quill/src/components/HelloWorld.vue
+++ vue-quill/src/components/HelloWorld.vue
@@ -33,6 +33,29 @@ export default {
       },
     };
   },
+  methods: {
+    onEditorBlur(quill) {
+      console.log("editor blur!", quill);
+    },
+    onEditorFocus(quill) {
+      console.log("editor focus!", quill);
+    },
+    onEditorReady(quill) {
+      console.log("editor ready!", quill);
+    },
+    onEditorChange({ quill, html, text }) {
+      console.log("editor change!", quill, html, text);
+      this.content = html;
+    },
+  },
+  computed: {
+    editor() {
+      return this.$refs.myQuillEditor.quill;
+    },
+  },
+  mounted() {
+    console.log("this is current quill instance object", this.editor);
+  },
 };
 </script>
 
