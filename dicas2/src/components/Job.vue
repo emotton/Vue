b5957160c5b865dd9675066d9a9e2cdc56fa8d2f
emotton <emotton@gmail.com>
2022-02-19 23:24:16 -0300

    Inclusão dicas2 - Injeção de dependências

---
+++ dicas2/src/components/Job.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Job',
+    provide(){
+        return {
+            getJobs: this.getJobs
+        }
+    },
+    methods: {
+        getJobs(){
+            return {
+                id: 1,
+                title: 'Example',
+                description: 'Example Injeção de Dependência'
+            }
+        }
+    },
+}
+</script>
---
+++ dicas2/src/components/JobTitle.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        JobTitle
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'JobTitle',
+    inject: ['getJobs'],
+    created() {
+        // console.log(this.$parent.$parent.getJobs());
+        console.log(this.getJobs());
+    },
+}
+</script>
