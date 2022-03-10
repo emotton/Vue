211dc1641d69a304156a6fd777315a1263d5ac97
emotton <emotton@gmail.com>
2022-03-09 23:50:46 -0300

    Inclusão do i18n

---
+++ vue-i18n-test/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ $t("message.title") }}</h1>
+    <h2>{{ $t("message.hello") }}</h2>
+    <div>
+      <button @click="changeLocale">Mudar para {{ locale }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      locale: "Português",
+    };
+  },
+  methods: {
+    changeLocale() {
+      this.$i18n.locale = this.$i18n.locale == "en" ? "pt" : "en";
+      this.locale = this.locale == "Inglês" ? "Português" : "Inglês";
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-i18n-test/src/i18n/messages.js
@@ -0,0 +1,16 @@
+const messages = {
+  en: {
+    message: {
+      title: "Welcome to Your Vue.js App",
+      hello: "hello world",
+    },
+  },
+  pt: {
+    message: {
+      title: "Bem vindo a sua aplicação Vue.js",
+      hello: "Olá Mundo",
+    },
+  },
+};
+
+export default messages;
